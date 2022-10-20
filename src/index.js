2ae7a89b85ff55306e596a7ce4c207109f15e0ee
Yann Darwish <yann.darwish@gmail.com>
2022-10-20 15:51:00 +0200

    css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import { UserIdProvider } from "./utils/context"
 import Router from "./router/Router"
 // css
 import "./index.css"
+import "./components/Header/Header.css"
+import "./components/LateralNav/LateralNav.css"
+import "./pages/Home/Home.css"
+import "./components/Recharts/ActivityChart/ActivityChart.css"
+import "./components/Recharts/AverageSessionsChart/AverageSessionsChart.css"
+import "./components/Recharts/PerformanceChart/PerformanceChart.css"
+import "./components/Recharts/ScoreChart/ScoreChart.css"
+import "./components/Card/Card.css"
+import "./components/Switch/Switch.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
